f2000211e4adc642c8bf6fd9c2ce2706e562a70b
Norbert Vígh <norbertusvigh@gmail.com>
2023-04-04 20:46:57 +0000

    fixing config for cloud server

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: ['localhost', 'migvis.norb.sk'],
+    client: {
+      webSocketURL: 'auto://0.0.0.0/ws'
+    }
+  }
 })
